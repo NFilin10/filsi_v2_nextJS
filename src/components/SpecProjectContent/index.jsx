aa7331082c33172ac485a3af378567261da8a888
nikita <nikita10filin@gmail.com>
2023-07-19 15:26:47 +0300

    import bugs

--- src/components/SpecProjectContent/index.jsx
+++ src/components/SpecProjectContent/index.jsx
@@ -4,7 +4,7 @@ import AOS from "aos";
 import Image from "next/image";
 import MainLayout from "@/layouts/MainLayout";
 
-const SpecProjectContent = (props) => {
+const Index = (props) => {
 
     useEffect(() => {
         AOS.init();
@@ -16,36 +16,37 @@ const SpecProjectContent = (props) => {
         <MainLayout>
             <div className={Styles.wrapper}>
                 <div className={Styles.header}>
-                    {/*<h1>{props.state.name}</h1>*/}
+
+                    <h1>{props.state.name ? props.state.name : ""}</h1>
                 </div>
                 <div className={Styles.projectImg}>
-                    <Image src={props.state.img}
+                    <Image src={props.state.img ? props.state.img : ""}
                            alt=""/>
                 </div>
                 <div className={Styles.content} data-aos="zoom-in-up">
                     <table className={Styles.table}>
                         <thead>
                         <tr>
-                            {/*<th colSpan="2"><h2>{props.state.name}</h2></th>*/}
+                            <th colSpan="2"><h2>{props.state.name ? props.state.name : ""}</h2></th>
                         </tr>
                         </thead>
                         <tbody className={Styles.tableBody}>
                         <tr>
                             <td className={Styles.label}>Lõpetamise kuupäev</td>
-                            <td>{props.state.finishDate}</td>
+                            <td>{props.state.finishDate ? props.state.finishDate : ""}</td>
                         </tr>
                         <tr>
                             <td className={Styles.label}>Asukoht</td>
-                            <td>{props.state.location}</td>
+                            <td>{props.state.location ? props.state.location : ""}</td>
                         </tr>
                         <tr>
                             <td className={Styles.label}>Tellija</td>
-                            <td>{props.state.completedBy}</td>
+                            <td>{props.state.completedBy ? props.state.completedBy : ""}</td>
                         </tr>
                         </tbody>
                     </table>
                     <p className={Styles.projectInfo}>
-                        {props.state.projectDescription}
+                        {props.state.projectDescription ? props.state.projectDescription : ""}
                     </p>
                 </div>
 
@@ -55,4 +56,4 @@ const SpecProjectContent = (props) => {
     )
 }
 
-export default SpecProjectContent
+export default Index
--- src/pages/projektid/[id].jsx
+++ src/pages/projektid/[id].jsx
@@ -1,29 +1,28 @@
 import React from 'react'
 import {useParams} from "react-router-dom";
-import SpecProjectContent from "@/pages/projektid/SpecProjectContent/SpecProjectContent";
 import state from "@/state";
 import {useRouter} from "next/router";
 import Head from "next/head";
+import Index from "src/components/SpecProjectContent";
 
 
 
 
 
 
-const Id = ({props}) => {
+const Id = (props) => {
 
-    const propsObj = JSON.parse(props)
+    // const propsObj = JSON.parse(props)
 
     const params = useRouter()
 
     let id = 0
-    for (const object of propsObj.home[0].projects) {
+    for (const object of props?.home[0].projects) {
         if (object.url === params.query.id){
             break
         }
         id++
     }
-
 
 
     return (
@@ -32,12 +31,12 @@ const Id = ({props}) => {
                 <meta charSet="UTF-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <meta name="author" content="Filsi"/>
-                <title>{propsObj.home[0].projects[id].name}</title>
-                <meta name="description" content={propsObj.home[0].projects[id].projectDescription}/>
+                <title>{props?.home[0].projects[id].name}</title>
+                <meta name="description" content={props?.home[0].projects[id].projectDescription}/>
                 <meta name="keywords" content="objekt, ehitustööd, projekt"/>
             </Head>
             <div>
-                <SpecProjectContent state={ propsObj.home[0].projects[id]}/>
+                <Index state={props?.home[0].projects[id]}/>
             </div>
         </>
 
@@ -47,10 +46,11 @@ export default Id
 
 
 export async function getStaticProps() {
-    const props = JSON.stringify(state)
+    // const props = JSON.stringify(state)
     return {
         props: {
-            props
+            // data: props
+            ...state
         }
     };
 }
